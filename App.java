52806501bffc611ea9279344b4cb7a6630e27f42
LBarros98 <lucas.barros.indrusiak@gmail.com>
2021-09-21 16:32:34 -0300

    Teste

--- App.java
+++ App.java
@@ -1,4 +1,8 @@
+//Buffet
+import java.util.Scanner;
 class App {
   public static void maid(String args []) {
     System.out.println("Meu segundo teste no git");
     System.out.println("Fim do programa");
+  }
+}
