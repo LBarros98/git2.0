df0dd56dc7b70a51c5cf465b25173b5a7827e796
LBarros98 <lucas.barros.indrusiak@gmail.com>
2021-09-21 16:34:53 -0300

    modificacao 2

--- App.java
+++ App.java
@@ -4,5 +4,6 @@ class App {
   public static void maid(String args []) {
     System.out.println("Meu segundo teste no git");
     System.out.println("Fim do programa");
+    System.out.println("SDD faé");
   }
 }
